a9ea71db702370d77a3fd7163aaa9c53fe6058c5
victor <e4f9p@ugrad.cs.ubc.ca>
2023-03-11 17:57:23 -0800

    Phase 2 Submission #2 (Fixed missing code coverage)

--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -50,12 +50,19 @@ public class JsonWriterTest extends JsonTest {
         try {
             WatchLists wl = new WatchLists();
             Movie batman = (new Movie("Batman", Status.TO_WATCH));
-            Show squidGame = (new Show("Squid Game", 9, 5, Status.WATCHING));
+            Movie ironMan = (new Movie("Iron Man", Status.WATCHING));
             Movie spiderman = (new Movie("Spiderman", Status.WATCHED));
+            Show breakingBad = (new Show("Breaking Bad", 62, 0, Status.TO_WATCH));
+            Show squidGame = (new Show("Squid Game", 9, 5, Status.WATCHING));
+            Show strangerThings = (new Show("Stranger Things", 8, 8, Status.WATCHED));
             spiderman.changeMovieRating(9);
+            strangerThings.changeShowRating(8);
             wl.addMovie(batman);
-            wl.addShow(squidGame);
+            wl.addMovie(ironMan);
             wl.addMovie(spiderman);
+            wl.addShow((breakingBad));
+            wl.addShow(squidGame);
+            wl.addShow(strangerThings);
             JsonWriter writer = new JsonWriter("./data/testWriterGeneralWatchList.json");
             writer.open();
             writer.write(wl);
@@ -63,12 +70,15 @@ public class JsonWriterTest extends JsonTest {
 
             JsonReader reader = new JsonReader("./data/testWriterGeneralWatchList.json");
             wl = reader.read();
-            assertEquals(1, wl.getToWatchList().size());
-            assertEquals(1, wl.getWatchingList().size());
-            assertEquals(1, wl.getWatchedList().size());
+            assertEquals(2, wl.getToWatchList().size());
+            assertEquals(2, wl.getWatchingList().size());
+            assertEquals(2, wl.getWatchedList().size());
             checkMovie("Batman", Status.TO_WATCH, 0, (Movie) wl.getToWatchList().get(0));
+            checkMovie("Iron Man", Status.WATCHING, 0, (Movie) wl.getWatchingList().get(0));
             checkMovie("Spiderman", Status.WATCHED, 9, (Movie) wl.getWatchedList().get(0));
-            checkShow("Squid Game", 9, 5, Status.WATCHING, 0, (Show) wl.getWatchingList().get(0));
+            checkShow("Breaking Bad",62, 0, Status.TO_WATCH, 0, (Show) wl.getToWatchList().get(1));
+            checkShow("Squid Game", 9, 5, Status.WATCHING, 0, (Show) wl.getWatchingList().get(1));
+            checkShow("Stranger Things", 8, 8, Status.WATCHED, 8, (Show) wl.getWatchedList().get(1));
         } catch (IOException e) {
             fail("Exception should not have been thrown");
         }
